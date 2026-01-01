9615fbe7b2560a98f85062a995c6c7cdc38f3ff2
Izak76 <izakalvesgama@outlook.com>
2026-01-01 15:30:04 -0300

    Função para ler grafo adicionada

--- utils.cpp
+++ utils.cpp
@@ -1,12 +1,35 @@
 // Aqui ficam funções e bibliotecas que funcionam em mais de um SO
 
-// TODO Colocar alguma coisa aqui...
+#include <bits/stdc++.h>
+#define fori(e) for (int i=0; i<e; i++)
+
+using namespace std;
+
+vector<vector<bool>> read_graph() {
+    int n, m, u, v;
+    cin >> n >> m;
+
+    vector<vector<bool>> adj(n);
+
+    fori(n) {
+        adj[i] = vector<bool>(n);
+        fill(adj[i].begin(), adj[i].end(), 0);
+    }
+
+    fori(m) {
+        cin >> u >> v;
+        adj[u][v] = true;
+        adj[v][u] = true;
+    }
+
+    return adj;
+}
 
 
 #ifdef _WIN32
 // Bibliotecas e versões de funções específicas para Windows
 
-#include <windows.h>
+// #include <windows.h>
 
 #elif __linux__
 // Bibliotecas e versões de funções específicas para Linux
--- utils.h
+++ utils.h
@@ -1,7 +1,10 @@
-// TODO Colocar alguma coisa aqui...
-
 /* 
     utils.h - Módulo de funções que:
     1 - São utilizadas por mais de um programa; ou
     2 - São implementadas de maneira diferente para cada sistema operacional
 */
+
+#include <vector>
+
+// Lê um grafo pela entrada padrão. O grafo segue o mesmo padrão proposto no trabalho
+std::vector<std::vector<bool>> read_graph();
