9998fe16f17e4569e7a3da4a0b9a601d8dfae062
Izak76 <izakalvesgama@outlook.com>
2026-01-10 08:44:28 -0300

    Ordenando importações

--- bmk.cpp
+++ bmk.cpp
@@ -1,8 +1,8 @@
 // Fortemente baseado em: https://www.geeksforgeeks.org/competitive-programming/hamiltonian-path-using-dynamic-programming/
 // Resolução do problema do caminho hamiltoniano usando programação dinâmica (algoritmo de Bellman–Held–Karp)
 
-#include <iostream>
 #include <cstring>
+#include <iostream>
 #include "utils/utils.h"
 #define forx(x, e) for (int x = 0; x < e; x++)
 
