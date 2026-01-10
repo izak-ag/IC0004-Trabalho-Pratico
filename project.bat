b660c3f42001ab4c149ade3b0dd072d4b24b0f08
Izak76 <izakalvesgama@outlook.com>
2026-01-09 21:55:43 -0300

    Mensagens de log adicionadas

--- project.bat
+++ project.bat
@@ -3,13 +3,21 @@
 set CPPVER=c++23 & rem Deve ser maior ou igual à 20
 
 IF "%1"=="build" (
+    echo Compilando "generate_test_cases.cpp" -^> "generator.exe"
     g++ generate_test_cases.cpp -std=%CPPVER% -o generator.exe
+
+    echo Compilando "bmk.cpp" -^> "bmk.exe"
     g++ utils/utils.cpp bmk.cpp -std=%CPPVER% -O2 -o bmk.exe
+
+    echo Compilando "sat.cpp" -^> "sat.exe"
     g++ utils/utils.cpp sat_solver/cdcl.cpp sat.cpp -std=%CPPVER% -O2 -o sat.exe
 )
 
 IF "%1"=="run" (
+    echo Gerando casos de testes
     generator
+
+    echo Executando experimento
 
     FOR %%F IN (testcases\*) DO (
         echo %%F -^> BMK
